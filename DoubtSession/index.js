aa3c92ec5b80444f9f49fc921efadc4c8979111c
farmantasleem <iamfarmanmalik@gmail.com>
2023-08-04 16:14:09 +0530

    JavaScript Basic

---
+++ DoubtSession/index.js
@@ -0,0 +1,82 @@
+// operators 
+
+
+// "=" --  assignment operator
+
+
+// let nameVar = "anything"
+
+// nameVar = "something";
+
+// Equal
+
+// Comparison Operator
+
+//"==" -->  value is same or not  return true/false
+//"===" --> is value & datatypes is same or not return boolean
+
+// "1" == 1  // value is same : true
+
+
+// "1" === 1  // value datatype is same : false
+
+// not equal
+// let yourName=5
+
+// greater | less than
+
+// greater or equal 
+
+// if(yourName<=5){
+//     console.log("its right!")
+// }else{
+//     console.log("Its wrong!")
+// }
+
+
+
+
+// logical operator 
+
+// let chai=5;
+// let cup=3;
+// let biscuits=5;
+
+//  and operator everycondition should be true
+
+// or operator -- if any condition is true so it will return true
+
+// if(chai==5 ||  cup=="5" && biscuits>=1){
+//     console.log("bring for students")
+// }else{
+//     console.log("prepare everything first")
+// }
+
+// 
+
+// let chai = 1
+
+
+
+
+const nameArray=[
+    {name:"Ashu",batch:"sigma2.0",money:100},
+    {name:"Rajnish",batch:"sigma2.0",money:200},
+    {name:"Himanshu",batch:"sigma 2.0",money:300},
+    {name:"Ashu",batch:"sigma 2.0",money:300},
+    {name:"Ashu",batch:"sigma 2.0",money:300},
+    {name:"Ashu",batch:"sigma 2.0",money:300},
+    {name:"Ashu",batch:"sigma 2.0",money:300},
+    
+]
+Ashu:10
+// i want to name of the person who has money more 250 rupees
+
+let howMany=0
+
+for(let i=0;i<nameArray.length;i++){
+    nameArray[i].name=="Ashu"?howMany++:""
+}
+
+console.log("Total Ashu",howMany)
+
